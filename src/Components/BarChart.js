ee60860038fbb49b8bc2846fc6b7785eaed147c4
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-11-12 17:41:11 -0500

    lab frontend

---
+++ src/Components/BarChart.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Chart from 'chart.js'
+
+const BarChart = () => {
+
+    React.useEffect(() => {
+        const makeAPICall = async () => {
+            try {
+                const res = await fetch('http://localhost:3000/stocks/1')
+                const json = await res.json()
+                console.log('BarChart - json', json)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        makeAPICall()
+    })
+
+    const prepareData = data => {
+        const chartData = {
+            labels: [],
+            datasets: [{label: 'average prices', data: []}]
+        }
+        data.average_prices.forEach(average_price => {
+            chartData.labels.push(average_price.id)
+            chartData.datasets[0].data.push(average_price.price)
+        })
+        console.log(chartData)
+        return chartData
+    }
+
+    return (
+        <>
+            <h1>Average Prices</h1>
+            <canvas id="averagePrices" width="300" height = "100"></canvas>
+        </>
+    )
+}
+
+export default BarChart
